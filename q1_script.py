0296f03ace6b5cc0964ce7cf2f2471dcd1694336
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2023-12-10 18:43:09 +0300

    used the q1 script to get the data in the PCA part

--- q1_script.py
+++ q1_script.py
@@ -1,12 +1,17 @@
 import numpy as np
 import gzip
+import matplotlib.pyplot as plt
 
 # Function to read pixel data from the dataset
 def read_pixels(data_path):
     with gzip.open(data_path) as f:
         pixel_data = np.frombuffer(f.read(), 'B', offset=16).astype('float32')
     normalized_pixels = pixel_data / 255
-    flattened_pixels = None # TODO: Flatten the normalized pixels
+    
+    data_size = normalized_pixels.shape[0]
+    image_count = int(data_size / 784)
+    
+    flattened_pixels = pixel_data.reshape(image_count, 784) # TODO: Flatten the normalized pixels
     return flattened_pixels
 
 # Function to read label data from the dataset
@@ -15,5 +20,21 @@ def read_labels(data_path):
         label_data = np.frombuffer(f.read(), 'B', offset=8)
     return label_data
 
-images = read_pixels("data/train-images-idx3-ubyte.gz")
-labels = read_labels("data/train-labels-idx1-ubyte.gz")
+def display_image(image, label):
+    plt.imshow(image.reshape(28, 28), cmap='gray')
+    plt.title("Label: " + str(label))
+    plt.show()
+
+if __name__ == "__main__":
+    images = read_pixels("data/data/train-images-idx3-ubyte.gz")
+    labels = read_labels("data/data/train-labels-idx1-ubyte.gz")
+
+    test_images = read_pixels("data/data/t10k-images-idx3-ubyte.gz")
+    test_labels = read_labels("data/data/t10k-labels-idx1-ubyte.gz")
+
+    print(test_images.shape)
+    print(test_labels.shape)
+
+    # display a random image with its label
+    num = np.random.randint(0, 10000)
+    display_image(test_images[num], test_labels[num])
